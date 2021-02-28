abec7ad6796fde71e66ce7b2ed8f17ab63c0face
stevescruz <steve.silverio.cruz@gmail.com>
2021-02-28 02:29:17 -0500

    REF: Server to parse request body with express.json

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import 'dotenv/config';
 import express from 'express';
 
 import '@shared/infra/typeorm/';
+import '@shared/container';
 import apiConfig from '@config/api';
 import routes from '@shared/infra/http/routes/routes';
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
 
 app.listen(apiConfig.port, () => {
