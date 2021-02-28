771c42b1036c49dbf02f182c8951a5635d2163be
stevescruz <steve.silverio.cruz@gmail.com>
2021-02-28 03:58:41 -0500

    REF: IUsersRepository included save method

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -4,6 +4,7 @@ import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<User>;
+  save(user: User): Promise<User>;
 };
 
 export default IUsersRepository;
